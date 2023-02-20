529570e1a030aa19cd95a8e726408bb03bb584f9
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-02-20 14:12:02 +0200

    added tailwind

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,3 +1,9 @@
 export default function MainPage() {
-    return <div>MainPage</div>;
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+        </div>
+    );
 }
