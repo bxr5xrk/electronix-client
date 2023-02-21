0eefeaf8fbfee8623491db9f08a158289ccc159c
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-02-21 14:59:30 +0200

    added watch list page

--- src/pages/WatchListPage/index.tsx
+++ src/pages/WatchListPage/index.tsx
@@ -1,3 +1,24 @@
+import { useAppSelector } from '../../app/store';
+import { selectWatchList } from '../../features/watchList/watchListSlice';
+import ProductItem from '../MainPage/components/ProductsList/components/ProductItem';
+
 export default function WatchListPage() {
-    return <div>WatchListPage</div>;
+    const { watchListItems } = useAppSelector(selectWatchList);
+
+    if (watchListItems.length === 0) {
+        return (
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <h1 className="text-2xl">No items in your watch list</h1>
+                <h3>Add one</h3>
+            </div>
+        );
+    }
+
+    return (
+        <section className="grid grid-cols-3 gap-3 overflow-y-scroll flex-grow">
+            {watchListItems?.map((product) => (
+                <ProductItem product={product} key={product.id} />
+            ))}
+        </section>
+    );
 }
