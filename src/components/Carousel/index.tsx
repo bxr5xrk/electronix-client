d6718f4f5ddb2fa75b2ef85ab95824f4cac65bdb
palyanytsya <vasyl.dobro.2003@gmail.com>
2023-03-06 13:10:43 +0200

    added animation for image switch icons

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -25,7 +25,7 @@ export default function Carousel({ images }: CarouselProps) {
     const onClickDot = (index: number) => setCurrentIndex(index);
 
     return (
-        <div className="relative flex justify-center items-center w-full h-full p-2">
+        <div className="group relative flex justify-center items-center w-full h-full p-2">
             {/* images */}
             <div className="relative flex items-center justify-center overflow-hidden w-full h-full">
                 {images.map((image, index) => (
@@ -47,7 +47,7 @@ export default function Carousel({ images }: CarouselProps) {
                     {/* previous chevron */}
                     <button
                         onClick={onPrevious}
-                        className="absolute left-2 bg-white hover:bg-gray-100 transition z-10 p-2 border rounded-full w-8 h-8 flex items-center justify-center"
+                        className="opacity-0 group-hover:opacity-100 absolute left-2 bg-white hover:bg-gray-100 transition z-10 p-2 border rounded-full w-8 h-8 flex items-center justify-center"
                     >
                         <ChevronLeftIcon className="w-5 h-5" aria-hidden />
                     </button>
@@ -55,13 +55,13 @@ export default function Carousel({ images }: CarouselProps) {
                     {/* next chevron */}
                     <button
                         onClick={onNext}
-                        className="absolute right-2 bg-white hover:bg-gray-100 transition z-10 p-2 border rounded-full w-8 h-8 flex items-center justify-center"
+                        className="opacity-0 group-hover:opacity-100 absolute right-2 bg-white hover:bg-gray-100 transition z-10 p-2 border rounded-full w-8 h-8 flex items-center justify-center"
                     >
                         <ChevronRightIcon className="w-5 h-5" aria-hidden />
                     </button>
 
                     {/* dots */}
-                    <div className="absolute flex gap-3 bottom-2">
+                    <div className="transition absolute flex gap-3 bottom-2">
                         {images.map((_, index) => (
                             <button
                                 key={`${_.href}${index}`}
